2a155e1dbfe5815f08cd4948e62f0173043f9857
Gejix <geraldjuwah@gmail.com>
2023-02-02 17:07:37 +0000

    [feat]: personal data added

---
+++ 0x00-personal_data/README.md
@@ -0,0 +1,8 @@
+# Personal Data
+
+Learning Objectives
+
+    - Examples of Personally Identifiable Information (PII)
+    - How to implement a log filter that will obfuscate PII fields
+    - How to encrypt a password and check the validity of an input password
+    - How to authenticate to a database using environment variables
---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+"""Handling user personal data
+"""
+import logging
+import mysql.connector
+from os import environ
+import re
+from typing import List
+
+
+PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
+
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """filter_datum that returns the log message obfuscated
+    Args
+        fields: a list of strings representing all fields to obfuscate
+        redaction: a string representing by what the field will be obfuscated
+        message: a string representing the log line
+        separator: a string representing by which character is separating
+        all fields in the log line (message)
+    Return (str): log message obfuscated
+    """
+    for field in fields:
+        message = re.sub(f'{field}=.*?{separator}',
+                         f'{field}={redaction}{separator}', message)
+    return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ Filters values in incoming log records using filter_datum """
+        record.msg = filter_datum(self.fields, self.REDACTION,
+                                  record.getMessage(), self.SEPARATOR)
+        return super(RedactingFormatter, self).format(record)
+
+
+def get_logger() -> logging.Logger:
+    """Create logger
+    """
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(RedactingFormatter(list(PII_FIELDS)))
+    logger.addHandler(stream_handler)
+
+    return logger
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """ Get database connection
+    """
+    host = environ.get('PERSONAL_DATA_DB_HOST')
+    user = environ.get('PERSONAL_DATA_DB_USERNAME')
+    password = environ.get('PERSONAL_DATA_DB_PASSWORD')
+    db = environ.get('PERSONAL_DATA_DB_NAME')
+
+    cur = mysql.connector.connection.MySQLConnection(
+          host=host, user=user, password=password, database=db)
+
+    return cur
